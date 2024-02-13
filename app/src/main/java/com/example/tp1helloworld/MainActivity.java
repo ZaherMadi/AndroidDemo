d81bd9dd61bdaddd0e661b7559b3e124e89cdbc3
Zaher Madi <zahermadi@yahoo.fr>
2024-02-13 16:19:08 +0100

    Pixel 3a Test Done

--- app/src/main/java/com/example/tp1helloworld/MainActivity.java
+++ app/src/main/java/com/example/tp1helloworld/MainActivity.java
@@ -6,14 +6,24 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.example.tp1helloworld.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity{
+
+
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //
-        //
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        TextView tv = new TextView(this);
+        tv.setText("Salut tout le monde !");
+        setContent View(tv);
+//binding.hellotv.setText("oui")
+
 
         Log.d("MainActivity: ",
         "onCreate");
